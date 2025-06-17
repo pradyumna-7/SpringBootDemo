621a08aaf042e0839ec9cd9cdca33768525a1f90
pradyumna-7 <pradyumna_7@outlook.com>
2025-06-17 11:45:56 +0530

    intial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+//@Configuration
+//public class StudentConfig {
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository) {
+//        return args -> {
+//            Student pradyumna = new Student("Pradyumna", 21, LocalDate.of(2003, 12, 11), "pradyumna_7@outlook.com");
+//            Student john = new Student("John", 25, LocalDate.of(2003, 12, 11), "john@outlook.com");
+//            repository.saveAll(List.of(pradyumna, john));
+//        };
+//    }
+//}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/student")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        Student savedStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
+}
